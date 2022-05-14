6c8f6698da24e40dbf19c8286247e75375e2e635
Cysecor <cysecor@gmail.com>
2022-05-14 12:25:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# validacija-forme" 
---
+++ style.css
@@ -0,0 +1,58 @@
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	font-family: sans-serif;
+	padding: 50px;
+}
+
+h1 {
+	text-align: center;
+	margin-bottom: 15px;
+}
+
+h2 {
+	margin-bottom: 15px;
+}
+
+h3 {
+	margin-bottom: 15px;
+}
+
+form {
+	max-width: 600px;
+	margin: auto;
+}
+
+form div {
+	padding: 15px 0;
+}
+
+form div label {
+	display: block;
+	font-weight: bold;
+	font-size: 16px;
+}
+
+form div input {
+	height: 50px;
+	width: 100%;
+	font-size: 18px;
+	padding: 0 10px;
+}
+
+form div button {
+	padding: 10px 30px;
+	font-size: 18px;
+}
+
+form ul {
+	list-style: none;
+}
+
+form ul li {
+	color: red;
+}
